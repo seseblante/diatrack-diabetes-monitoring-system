fb4b0cb1e68ea1efe6b18bb570af8edd78da44ca
Arianne Acosta <adacosta3@up.edu.ph>
2025-09-19 03:10:28 +0800

    First commit!

---
+++ hub-backend/src/test/java/app/hub_backend/HubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
